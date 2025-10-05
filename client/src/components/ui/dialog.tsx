3738263f0c3283728d271110c2a9e82147da4330
Musaddique <musajs91@gmail.com>
2025-10-06 00:52:48 +0400

    fix db

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -38,9 +38,11 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "fixed left-1/2 top-1/2 z-50 grid w-[90%] max-w-full h-[90vh] max-h-[90vh] -translate-x-1/2 -translate-y-1/2 gap-4 border bg-background p-6 shadow-lg duration-200 sm:w-[80%] sm:max-w-lg sm:rounded-lg md:w-[60%] md:max-w-2xl lg:w-[50%] lg:max-w-3xl xl:w-[40%] xl:max-w-4xl overflow-y-auto",
         className
       )}
+      
+      
       {...props}
     >
       {children}
